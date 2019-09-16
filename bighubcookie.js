1c8593c08a67f2edf68aba14ab15266946619117
jiura <jovicsou@gmail.com>
2019-09-16 15:18:33 -0300

    Add files via upload

--- bighubcookie.js
+++ bighubcookie.js
@@ -88,7 +88,7 @@ function create_cookie() {
 
 //Defines the .serializeObject() JQuery function
 function define_serializeObject() {
-    //$a = jQuery.noConflict();
+    var $a = jQuery.noConflict();
     $a.fn.serializeObject = function () {
         var o = {};
         var a = this.serializeArray();
